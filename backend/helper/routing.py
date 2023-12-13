d40e6f683f4c6d96024df8192ab5b54c255ea85e
Baudouin Dafflon <baudouin.dafflon@ecov.fr>
2023-12-13 22:07:30 +0100

    demo

--- backend/helper/routing.py
+++ backend/helper/routing.py
@@ -3,7 +3,7 @@ from openrouteservice.directions import directions
 from helper.math import distance
 
 def get_route(origine, destination):
-    client = openrouteservice.Client(key = "5b3ce3597851110001cf6248d5f91ac8b85043b3ba1584a754423c88")
+    client = openrouteservice.Client(key = "")
     route = directions(client, ([origine[1],origine[0]],
                                     [destination[1],destination[0]]),
                            profile='driving-car', preference="fastest")
